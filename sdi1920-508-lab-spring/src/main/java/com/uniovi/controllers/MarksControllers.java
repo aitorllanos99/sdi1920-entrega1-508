5710410b0098c8c34fba40fc01ee26267a08e5f4
aitorllanos99 <UO264476@gmail.com>
2020-02-22 10:48:15 +0100

    SDI-SpringBoot-Sesión-4.4-Consulta

--- sdi1920-508-lab-spring/src/main/java/com/uniovi/controllers/MarksControllers.java
+++ sdi1920-508-lab-spring/src/main/java/com/uniovi/controllers/MarksControllers.java
@@ -1,7 +1,6 @@
 package com.uniovi.controllers;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.security.Principal;
 
 import javax.servlet.http.HttpSession;
 
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.uniovi.entities.Mark;
+import com.uniovi.entities.User;
 import com.uniovi.services.MarkService;
 import com.uniovi.services.UsersService;
 import com.uniovi.validators.AddMarksValidator;
@@ -33,9 +33,19 @@ public class MarksControllers {
 	private HttpSession httpSession;
 
 	@RequestMapping("/mark/list")
-	public String getList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+	public String getList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 		return "mark/list";
+	}
+
+	@RequestMapping("/mark/list/update")
+	public String updateList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
+		return "mark/list :: tableMarks";
 	}
 
 	@RequestMapping(value = "/mark/add", method = RequestMethod.GET)
@@ -88,11 +98,7 @@ public class MarksControllers {
 		return "redirect:/mark/details/" + id;
 	}
 
-	@RequestMapping("/mark/list/update")
-	public String updateList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
-		return "mark/list :: tableMarks";
-	}
+
 
 	@RequestMapping(value = "/mark/{id}/resend", method = RequestMethod.GET)
 	public String setResendTrue(Model model, @PathVariable Long id) {
